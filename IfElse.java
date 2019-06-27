9288f3eaace334916ec4445d93e30c46c491c7c6
Lv, Ranran （吕冉冉） <Ranran.Lv@elekta.com>
2019-06-27 19:45:56 +0800

    learn java

---
+++ IfElse.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class IfElse {
+    static int result = 0;
+
+    static void test(int testval, int target) {
+        if (testval > target) {
+            result += 1;
+        } else if (testval < target) {
+            result -= 1;
+        } else {
+            result = 0;
+        }
+    }
+
+    public static void main(String[] agrs) {
+        test(10, 5);
+        System.out.println(result);
+        test(5, 10);
+        System.out.println(result);
+        test(5, 5);
+        System.out.println(result);
+    }
+}
---
+++ InitializationVerification.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class InitializationVerification {
+    private static int a;
+    private static char b;
+
+    public static void main(String[] args) {
+        System.out.println(a);
+        System.out.println(b);
+        System.exit(0);
+    }
+}
