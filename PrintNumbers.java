2cf00ece8e89fac0fb73bb54ba2e1a21fd266ffe
LVRANRAN <m13871193221@163.com>
2019-06-28 10:41:55 +0800

    learn java

---
+++ PrintNumbers.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class PrintNumbers {
+    public static void main(String[] agrs) {
+        for (int i = 0; i <= 100; i++)
+            System.out.println(i);
+    }
+}
---
+++ WhileTest.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class WhileTest {
+    static boolean condition() {
+        boolean result = Math.random() < 0.99;
+        System.out.print(result + ", ");
+        return result;
+    }
+
+    public static void main(String[] agrs) {
+        while (condition())
+            System.out.println("Inside 'while'");
+        System.out.println("Outside 'while'");
+    }
+}
